eccfc5d6837646e57ed7817d41d26ca2ecffd0a7
Max <maxmillianrwoods@gmail.com>
2019-06-18 11:41:56 -0700

    working on chaining

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,38 @@
 
 var fs = require('fs');
 var request = require('request');
+const readline = require('readline');  
+
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      callback(err);
+    } else {
+      data = data.toString().split("\n");
+      data.length = 1;
+      console.log(data[0]);
+      callback(null, data[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, function (err, response) {
+    if (err) {
+      console.error(err);
+      callback(err);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      callback(null, response.statusCode);
+    }
+  
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('request');
@@ -9,54 +9,65 @@ var crypto = require('crypto');
 var Promise = require('bluebird');
 
 // (1) Asyncronous HTTP request
-var getGitHubProfile = function(user, callback) {
+var getGitHubProfile = function (user, callback) {
   var options = {
     url: 'https://api.github.com/users/' + user,
-    headers: { 'User-Agent': 'request' },
-    json: true  // will JSON.parse(body) for us
+    headers: {
+      'User-Agent': 'request'
+    },
+    json: true // will JSON.parse(body) for us
   };
 
-  request.get(options, function(err, res, body) {
-    if (err) {
-      callback(err, null);
-    } else if (body.message) {
-      callback(new Error('Failed to get GitHub profile: ' + body.message), null);
-    } else {
-      callback(null, body);
-    }
+  return new Promise((resolve, reject) => {
+    request.get(options, function (err, res, body) {
+      if (err) {
+        reject(err);
+      } else if (body.message) {
+        reject(new Error('Failed to get GitHub profile: ' + body.message), null);
+      } else {
+        resolve(body);
+      }
+    });
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = getGitHubProfile; // TODO
 
 
 // (2) Asyncronous token generation
-var generateRandomToken = function(callback) {
-  crypto.randomBytes(20, function(err, buffer) {
-    if (err) { return callback(err, null); }
-    callback(null, buffer.toString('hex'));
+var generateRandomToken = function (callback) {
+  return new Promise( (resolve, reject) => {
+    crypto.randomBytes(20, function (err, buffer) {
+      if (err) {
+        reject(err);
+      }
+      resolve(buffer.toString('hex'));
+    });
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = generateRandomToken; // TODO
 
 
 // (3) Asyncronous file manipulation
-var readFileAndMakeItFunny = function(filePath, callback) {
-  fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
-   
-    var funnyFile = file.split('\n')
-      .map(function(line) {
-        return line + ' lol';
-      })
-      .join('\n');
-
-    callback(funnyFile);
+var readFileAndMakeItFunny = function (filePath, callback) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function (err, file) {
+      if (err) {
+        reject(err);
+      } else {
+        var funnyFile = file.split('\n')
+          .map(function (line) {
+            return line + ' lol';
+          })
+          .join('\n');
+        resolve(funnyFile);
+      }
+    });
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = readFileAndMakeItFunny; // TODO
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
